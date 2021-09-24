f4f5b338ff1e014fe3eab7241019dcda62917696
127.0.0.1 <admin@samsesh.net>
2021-09-24 17:18:01 +0330

    add first file

---
+++ run.py
@@ -0,0 +1,26 @@
+import requests
+api = "https://www.clubhouseapi.com/api/"
+usertoken = input ("Enter your Token : ")
+action = input("Enter your action: ")
+code = input("Enter channel code: ")
+url = (api+action)
+
+data = "{\r\n \"channel\":\"%s\"\r\n}" % code
+print (data)
+for i in range(1):
+ headers = {
+ 'CH-Languages': 'en-US',
+ 'CH-Locale': 'en_US',
+ 'Accept': 'application/json',
+ 'Accept-Encoding': 'gzip, deflate',
+ 'CH-AppBuild': '588',
+ 'CH-AppVersion': '1.0.10',
+ 'CH-UserID': '736014831',
+ 'User-Agent': 'clubhouse/588 (iPhone; iOS 15; Scale/2.00)',
+ 'Connection': 'close',
+ 'Content-Type': 'application/json; charset=utf-8',
+ 'Authorization': 'Token '+usertoken
+ }
+#print (url, headers)
+#response = requests.request ("POST", url, headers=headers, data=data)
+#print (response.text)
